2d30bc33e887434ff4b5b1cb2937eba447184c02
apurvc99 <apurv@venwiz.com.com>
2022-01-30 17:08:17 +0530

    quest 6 - complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { airDropHelper } from "./helper/airDrop.helper";
+import { initialMintHelper } from "./helper/initialMint.helper";
 import { walletConnectionHelper } from "./helper/walletConnection.helper";
 
 const App = () => {
@@ -7,9 +8,14 @@ const App = () => {
   const [provider, setProvider] = useState();
   const [loading, setLoading] = useState();
 
+  const [isTokenCreated, setIsTokenCreated] = useState(false);
+  const [createdTokenPublicKey, setCreatedTokenPublicKey] = useState(null);
+  const [mintingWalletSecretKey, setMintingWalletSecretKey] = useState(null);
+
   return (
     <div>
       <h1>Create your own token using JavaScript</h1>
+      {/* Connecting with Phantom Wallet */}
       {walletConnected && (
         <p>
           <strong>Public Key:</strong> {provider.publicKey.toString()}
@@ -27,15 +33,35 @@ const App = () => {
       >
         {!walletConnected ? "Connect Wallet" : "Disconnect Wallet"}
       </button>
-
+      {/* AIR_DROP yourself some SOL */}
       {walletConnected && (
         <p>
-          Airdrop 1 SOL into your wallet
+          Airdrop 1 SOL into your wallet{" "}
           <button
             disabled={loading}
             onClick={() => airDropHelper(setLoading, provider)}
           >
             AirDrop SOL{" "}
+          </button>
+        </p>
+      )}
+      {/* Minting your Tokens */}
+      {walletConnected && (
+        <p>
+          Create your own token
+          <button
+            disabled={loading}
+            onClick={() =>
+              initialMintHelper(
+                provider,
+                setLoading,
+                setIsTokenCreated,
+                setCreatedTokenPublicKey,
+                setMintingWalletSecretKey
+              )
+            }
+          >
+            Initial Mint{" "}
           </button>
         </p>
       )}
---
+++ src/helper/initialMint.helper.js
@@ -0,0 +1,82 @@
+import {
+  Keypair,
+  Transaction,
+  sendAndConfirmTransaction,
+  Connection,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
+export const initialMintHelper = async (
+  provider,
+  setLoading,
+  setIsTokenCreated,
+  setCreatedTokenPublicKey,
+  setMintingWalletSecretKey
+) => {
+  try {
+    setLoading(true);
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    const mintRequester = await provider.publicKey;
+    const mintingFromWallet = await Keypair.generate();
+    setMintingWalletSecretKey(JSON.stringify(mintingFromWallet.secretKey));
+
+    const fromAirDropSignature = await connection.requestAirdrop(
+      mintingFromWallet.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature, {
+      commitment: "confirmed",
+    });
+
+    const creatorToken = await Token.createMint(
+      connection,
+      mintingFromWallet,
+      mintingFromWallet.publicKey,
+      null,
+      6,
+      TOKEN_PROGRAM_ID
+    );
+    const fromTokenAccount =
+      await creatorToken.getOrCreateAssociatedAccountInfo(
+        mintingFromWallet.publicKey
+      );
+    await creatorToken.mintTo(
+      fromTokenAccount.address,
+      mintingFromWallet.publicKey,
+      [],
+      1000000
+    );
+
+    const toTokenAccount = await creatorToken.getOrCreateAssociatedAccountInfo(
+      mintRequester
+    );
+    const transaction = new Transaction().add(
+      Token.createTransferInstruction(
+        TOKEN_PROGRAM_ID,
+        fromTokenAccount.address,
+        toTokenAccount.address,
+        mintingFromWallet.publicKey,
+        [],
+        1000000
+      )
+    );
+    const signature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [mintingFromWallet],
+      { commitment: "confirmed" }
+    );
+
+    console.log("SIGNATURE:", signature);
+
+    setCreatedTokenPublicKey(creatorToken.publicKey.toString());
+    setIsTokenCreated(true);
+    setLoading(false);
+  } catch (err) {
+    console.log(err);
+    setLoading(false);
+  }
+};
